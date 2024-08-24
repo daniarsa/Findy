2ea54dcdc70fe1d9015bcba76cd66c9572cbe8de
Sara Jaramillo <sarasunrise45@gmail.com>
2024-08-24 15:44:46 -0500

    Se actualizan las funciones para llamar a los elementos desde el json

--- src/services/findyServices.js
+++ src/services/findyServices.js
@@ -1,9 +1,9 @@
 import endpoints from "../config";
 import axios from "axios";
 
-export const getUsers = async () => {
+export const getFeed = async () => {
     try {
-        const response = await axios.get(endpoints.users);
+        const response = await axios.get(endpoints.feed);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -11,9 +11,19 @@ export const getUsers = async () => {
     }
 };
 
-export const getPost = async () => {
+export const getProfile = async () => {
     try {
-        const response = await axios.get(endpoints.posts);
+        const response = await axios.get(endpoints.profile);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+export const getImageDetail = async () => {
+    try {
+        const response = await axios.get(endpoints.imageDetail); response
         return response.data;
     } catch (error) {
         console.error(error);
