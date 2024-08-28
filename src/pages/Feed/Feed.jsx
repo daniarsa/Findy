358501e81e5ef68666f4d0705f4b84d2c7fdaaf1
Angie Liceth <liceth5262@gmail.com>
2024-08-27 22:11:00 -0500

    en las stories se agrego el + al your story

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -5,6 +5,7 @@ import { FaRegHeart, FaRegComment } from "react-icons/fa";
 import { FaRegComments } from "react-icons/fa6";
 import { TbLocationShare } from "react-icons/tb";
 import { FaBookmark } from "react-icons/fa6";
+import { MdOutlineAdd } from "react-icons/md";
 
 const Feed = () => {
   const [feedData, setFeedData] = useState(null);
@@ -38,7 +39,13 @@ const Feed = () => {
         {feedData.stories && feedData.stories.length > 0 ? (
           feedData.stories.map((story, index) => (
             <div key={index} className="story flex flex-col justify-items-center">
-              <img src={story.image2} alt={story.title} className=" mx-auto h-16 w-16 object-cover inline-block rounded-full p-[2.5px]  bg-gradient-to-r from-color-1 via-color-2 to-color-4 " />
+              {/* Mostrar el ícono MdOutlineAdd solo para el primer story con title "Your Story" */}
+              {story.title === "Your Story" && index === 0 && (
+                <div className="absolute top-[103px] left-[221px] ">
+                  <MdOutlineAdd className="text-color-5 w-7 h-7 font-bold" />
+                </div>
+              )}
+              <img src={story.image2} alt={story.title} className="mx-auto h-16 w-16 object-cover inline-block rounded-full p-[2.5px] bg-gradient-to-r from-color-1 via-color-2 to-color-4" />
               <p className="text-xs font-balsamiq my-1">{story.title}</p>
             </div>
           ))
@@ -56,7 +63,7 @@ const Feed = () => {
                     <img
                       src={story1.image2}
                       alt={story1.title}
-                      className=" h-7 w-7 object-cover inline-block rounded-full p-[1.5px]  bg-gradient-to-r from-color-1 via-color-2 to-color-4"
+                      className="h-7 w-7 object-cover inline-block rounded-full p-[1.5px] bg-gradient-to-r from-color-1 via-color-2 to-color-4"
                     />
                     <p className="font-balsamiq font-bold">{story1.title}</p>
                   </div>
@@ -66,14 +73,14 @@ const Feed = () => {
                     <img
                       src={story2.image2}
                       alt={story2.title}
-                      className=" h-7 w-7 object-cover inline-block rounded-full p-[2.5px]  bg-gradient-to-r from-color-1 via-color-2 to-color-4 "
+                      className="h-7 w-7 object-cover inline-block rounded-full p-[2.5px] bg-gradient-to-r from-color-1 via-color-2 to-color-4 "
                     />
                     <p className="font-balsamiq font-bold">{story2.title}</p>
                   </div>
                 )}
               </div>
               <div className="flex justify-center items-center">
-                <img src={post.image} alt="" className=" rounded-lg w-96 h-96 object-cover" />
+                <img src={post.image} alt="" className="rounded-lg w-96 h-96 object-cover" />
               </div>
               <div className="px-4 pt-4 pb-2">
                 <div className="flex flex-row justify-between items-start">
